3088282eee5ddca3336c334344200a262144c6c1
luk <luk@lenoweb.be>
2023-12-30 20:11:40 +0100

    t1:16:13

---
+++ app/components/Newest.tsx
@@ -0,0 +1,27 @@
+import { simplifiedProduct } from "../interface";
+import { client } from "../lib/sanity";
+async function getData() {
+  const query = `*[_type == 'product'][0...4] {
+        _id,
+          price,
+          name,
+          "slug": slug.current,
+          "categoryName": category->name,
+          "imageUrl": images[0].asset->url
+      }`;
+  const data = await client.fetch(query);
+}
+
+export default async function Newest() {
+  const data: simplifiedProduct[] = await getData();
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="flex justify-between items-center">
+          <h2>Our Newest products</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
