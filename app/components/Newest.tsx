44fa9e3587035341edc7ed8f05408072a01d1a4d
luk <luk@lenoweb.be>
2023-12-30 20:15:00 +0100

    t1:17:25

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -19,7 +19,10 @@ export default async function Newest() {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between items-center">
-          <h2>Our Newest products</h2>
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Our Newest products
+          </h2>
+          <link href="/all">See all</link>
         </div>
       </div>
     </div>
