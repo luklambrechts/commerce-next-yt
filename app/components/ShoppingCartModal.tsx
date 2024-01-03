f2e41f32ac516111a57e6fb4eb876bd9f1086928
luk <luk@lenoweb.be>
2024-01-03 11:43:48 +0100

    t2:33:12

--- app/components/ShoppingCartModal.tsx
+++ app/components/ShoppingCartModal.tsx
@@ -14,6 +14,7 @@ export default function ShoppingCartModal() {
     handleCartClick,
     cartDetails,
     removeItem,
+    totalPrice,
   } = useShoppingCart();
   return (
     <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
@@ -23,49 +24,57 @@ export default function ShoppingCartModal() {
         </SheetHeader>
         <div className="h-full flex flex-col justify-between">
           <div className="mt-8 flex-1 overflow-y-auto">
-            <ul className="-my-6 divide-y divide-gray-200"></ul>
-            {cartCount === 0 ? (
-              <h1 className="py-6">You dont have any items</h1>
-            ) : (
-              <>
-                {Object.values(cartDetails ?? {}).map((entry) => (
-                  <li key={entry.id} className="flex py-6">
-                    <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                      <Image
-                        src={entry.image as string}
-                        alt="Product Image"
-                        width={100}
-                        height={100}
-                      />
-                    </div>
-                    <div className="ml-4 flex flex-1 flex-col">
-                      <div>
-                        <div className="flex justify-between text-base font-medium text-gray-900">
-                          <h3>{entry.name}</h3>
-                          <p className="ml-4">${entry.price}</p>
-                        </div>
+            <ul className="-my-6 divide-y divide-gray-200">
+              {cartCount === 0 ? (
+                <h1 className="py-6">You dont have any items</h1>
+              ) : (
+                <>
+                  {Object.values(cartDetails ?? {}).map((entry) => (
+                    <li key={entry.id} className="flex py-6">
+                      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                        <Image
+                          src={entry.image as string}
+                          alt="Product Image"
+                          width={100}
+                          height={100}
+                        />
+                      </div>
+                      <div className="ml-4 flex flex-1 flex-col">
+                        <div>
+                          <div className="flex justify-between text-base font-medium text-gray-900">
+                            <h3>{entry.name}</h3>
+                            <p className="ml-4">${entry.price}</p>
+                          </div>
 
-                        <p className="mt-1 text-sm test-gray-500 line-clamp-2">
-                          {entry.description}
-                        </p>
-                      </div>
-                      <div className="flex flex-1 items-end justify-between text-sm">
-                        <p className="text-gray-500">QTY: {entry.quantity}</p>
-                        <div className="flex">
-                          <button
-                            type="button"
-                            onClick={() => removeItem(entry.id)}
-                            className="font-medium text-primary hover:text-primary/80"
-                          >
-                            Remove
-                          </button>
+                          <p className="mt-1 text-sm test-gray-500 line-clamp-2">
+                            {entry.description}
+                          </p>
+                        </div>
+                        <div className="flex flex-1 items-end justify-between text-sm">
+                          <p className="text-gray-500">QTY: {entry.quantity}</p>
+                          <div className="flex">
+                            <button
+                              type="button"
+                              onClick={() => removeItem(entry.id)}
+                              className="font-medium text-primary hover:text-primary/80"
+                            >
+                              Remove
+                            </button>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                ))}
-              </>
-            )}
+                    </li>
+                  ))}
+                </>
+              )}
+            </ul>
+          </div>
+
+          <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+            <div className="flex justify-between text-base font-medium text-gray-900">
+              <p>Subtotal</p>
+              <p>${totalPrice}</p>
+            </div>
           </div>
         </div>
       </SheetContent>
