aa3c11d0e9428c659b3455c9a9ebaab5650efa66
luk <luk@lenoweb.be>
2024-01-03 12:30:07 +0100

    t2:45:07 checkout will not work comment author

---
+++ app/components/CheckoutOutNow.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useShoppingCart } from "use-shopping-cart";
+import { urlFor } from "../lib/sanity";
+import { ProductCart } from "./AddToBag";
+
+export default function CheckoutNow({
+  currency,
+  description,
+  image,
+  name,
+  price,
+  price_id,
+}: ProductCart) {
+  const { checkoutSingleItem } = useShoppingCart();
+  function buyNow(priceId: string) {
+    checkoutSingleItem(priceId);
+  }
+  const product = {
+    name,
+    description,
+    price,
+    currency,
+    image: urlFor(image).url(),
+    price_id: price_id,
+    id: "sldmjflsdjf",
+  };
+  return (
+    <Button
+      onClick={() => {
+        buyNow(product.price_id);
+      }}
+    >
+      Add To Cart
+    </Button>
+  );
+}
