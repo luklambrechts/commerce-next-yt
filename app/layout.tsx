da8a26669e7d182b4d556d048c6a0ca7640c45b7
luk <luk@lenoweb.be>
2024-01-03 16:58:14 +0100

    navbar fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/navBar";
+import NavBar from "./components/NavBar";
 import CartProvider from "./components/Providers";
 import ShoppingCartModal from "./components/ShoppingCartModal";
 import { ShoppingCart } from "lucide-react";
